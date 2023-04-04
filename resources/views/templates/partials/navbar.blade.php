1479db11ea33218ec2c3e2b984d9792604960a89
rachmanj <rachmanj@gmail.com>
2023-04-04 15:13:40 +0800

    minor update

--- resources/views/templates/partials/navbar.blade.php
+++ resources/views/templates/partials/navbar.blade.php
@@ -3,7 +3,7 @@
   <div class="container">
     <a href="#"class="navbar-brand">
       <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text text-white font-weight-light">Payreq - Support</span>
+      <span class="brand-text text-white font-weight-light">Vendor <small>Info</small></span>
     </a>
 
     <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
